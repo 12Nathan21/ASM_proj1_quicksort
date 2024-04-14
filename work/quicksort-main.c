bc16394e1cba427f9acfbf01abd759c3ba5d5710
ADVM <wangjingyang21@mails.ucas.ac.cn>
2024-04-14 18:02:51 +0800

    Add echo $i in .sh

--- work/quicksort-main.c
+++ work/quicksort-main.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <sys/time.h>
 
 #define MAX_SIZE 10000
 #define LB (0 - MAX_SIZE << 2) 
@@ -42,7 +42,7 @@ void check(int test[], int trust[], int size)
 	for(int i = 0; i < size; i++) {
 		if(test[i] != trust[i]) {
 			printf("ARR[%d] SHOULD BE %d, BUT GOT %d.\n", i, trust[i], test[i]);	
-			printf("TEST FALIED\n");
+			printf("TEST FAILED\n");
 			return;
 		}
 	}
@@ -65,6 +65,14 @@ void print_array(int arr[], int size)
 
 int main()
 {
+	float c_duration = 0;
+	struct timespec c_start_time = {0, 0};
+	struct timespec c_end_time = {0, 0};
+	float asm_duration = 0;
+	struct timespec asm_start_time = {0, 0};
+	struct timespec asm_end_time = {0, 0};
+	int Converter = 1000 * 1000 * 1000;
+
 	int *array_asm= malloc(sizeof(int) * MAX_SIZE);
 	fill_array_random(array_asm, MAX_SIZE, LB, UB);
 
@@ -72,12 +80,24 @@ int main()
 	int *array_c= malloc(sizeof(int) * MAX_SIZE);
 	memcpy(array_c, array_asm, sizeof(int) * MAX_SIZE);
 #endif
-
+	clock_gettime(CLOCK_REALTIME, &asm_start_time);
 	quicksort_asm(array_asm, 0, MAX_SIZE - 1);
+	clock_gettime(CLOCK_REALTIME, &asm_end_time);
+	asm_duration = (asm_end_time.tv_sec * Converter + asm_end_time.tv_nsec) 
+			   - (asm_start_time.tv_sec * Converter + asm_start_time.tv_nsec);
+	printf("asm qsort time: %f us\n",asm_duration/1000);
 
-#ifdef TEST 
+#ifdef TEST
+	clock_gettime(CLOCK_REALTIME, &c_start_time);
+
 	quicksort_c(array_c, 0, MAX_SIZE - 1);
+
+	clock_gettime(CLOCK_REALTIME, &c_end_time);
+	c_duration = (c_end_time.tv_sec * Converter + c_end_time.tv_nsec) 
+			   - (c_start_time.tv_sec * Converter + c_start_time.tv_nsec);
+	printf("c qsort time: %f us\n",c_duration/1000);
 	check(array_asm, array_c, MAX_SIZE);
+	printf("asm time / c time : %f\n\n",asm_duration/c_duration);
 #endif
 	// print_array(array_asm, MAX_SIZE);	
 	return 0;
--- work/quicksort.sh
+++ work/quicksort.sh
@@ -3,5 +3,6 @@ as -gstabs -o partition.o partition.s
 as -gstabs -o quicksort.o quicksort.s
 gcc -g -static -DTEST -o quicksort partition.o quicksort.o quicksort-main.c
 for i in {1..25}; do
+    echo $i
     ./quicksort
 done
